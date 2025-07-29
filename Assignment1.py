bcaacf8cf3aae4980e379d392811ebb503cad731
AMRENDRA-108 <kumaramrendra1512023@gmail.com>
2025-07-29 13:39:01 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,252 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "7df0a863",
+   "metadata": {},
+   "source": [
+    "# Task 1"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "f5c0c360",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enter first number10\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = int(input(\"enter first number\"))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "4a0aa4af",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enter second number5\n"
+     ]
+    }
+   ],
+   "source": [
+    "b = int(input(\"enter second number\"))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "809ebaa8",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "c = a+b;\n",
+    "d = a-b;\n",
+    "e = a*b;\n",
+    "f = a/b;"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "d2237fd8",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Addition :  15\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Addition : \",c)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "57513be1",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Subtraction : 5\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Subtraction :\",d)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "e7bee941",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Multiplication :  50\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Multiplication : \",e)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "5f33b621",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Division :  2.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Division : \",f)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "1ba21ab3",
+   "metadata": {},
+   "source": [
+    "# Task 2"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "05881540",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter first name :Binod\n"
+     ]
+    }
+   ],
+   "source": [
+    "f_name = str(input(\"Enter first name :\"))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "94279d4b",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter last name :kumar\n"
+     ]
+    }
+   ],
+   "source": [
+    "l_name = str(input(\"Enter last name :\"))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "bd2164ae",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "full_name = (f_name +\" \"+ l_name)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "6e110bb2",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Binod kumar\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(full_name)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 16,
+   "id": "87f9f555",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello,  Binod kumar ! Welcome to the digital world.\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello, \",full_name,\"! Welcome to the digital world.\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3abcba25",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
